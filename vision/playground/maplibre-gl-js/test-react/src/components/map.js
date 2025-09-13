0296218c62e3b19852c56c60c9658575557bda2c
Gareth Stockwell <gareth.stockwell@gmail.com>
2025-09-13 23:04:32 +0100

    test-react: add MapLibre example
    Based on https://docs.maptiler.com/react/maplibre-gl-js/how-to-use-maplibre-gl-js/

---
+++ vision/playground/maplibre-gl-js/test-react/src/components/map.js
@@ -0,0 +1,27 @@
+import React, { useRef, useEffect } from 'react';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import './map.css';
+
+export default function Map() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+
+  useEffect(() => {
+    if (map.current) return; // stops map from intializing more than once
+
+    map.current = new maplibregl.Map({
+      container: mapContainer.current,
+      style: 'https://demotiles.maplibre.org/globe.json', // style URL
+      center: [0, 0], // starting position [lng, lat]
+      zoom: 1 // starting zoom
+    });
+
+  });
+
+  return (
+    <div className="map-wrap">
+      <div ref={mapContainer} className="map" />
+    </div>
+  );
+}
