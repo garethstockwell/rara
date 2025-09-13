63e7af5abd3ba600c90328405531cd9d4db21079
Gareth Stockwell <gareth.stockwell@gmail.com>
2025-09-13 22:50:18 +0100

    test-cdn: MapLibre example via CDN

---
+++ vision/playground/maplibre-gl-js/test-cdn/index.js
@@ -0,0 +1,11 @@
+// From https://maplibre.org/maplibre-gl-js/docs/
+
+//import maplibregl from 'maplibre-gl';
+//import 'maplibre-gl/dist/maplibre-gl.css';
+
+const map = new maplibregl.Map({
+    container: 'map', // container id
+    style: 'https://demotiles.maplibre.org/globe.json', // style URL
+    center: [0, 0], // starting position [lng, lat]
+    zoom: 1 // starting zoom
+});
