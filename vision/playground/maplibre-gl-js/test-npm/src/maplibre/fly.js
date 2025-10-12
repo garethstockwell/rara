398ceb9f4c74092a507716bf2d8068790fe8b38a
Gareth Stockwell <gareth.stockwell@gmail.com>
2025-10-12 22:12:57 +0100

    test-npm: add 3D buildings

--- vision/playground/maplibre-gl-js/test-npm/src/maplibre/fly.js
+++ vision/playground/maplibre-gl-js/test-npm/src/maplibre/fly.js
@@ -25,7 +25,7 @@ export function createMap() {
                     "properties": {},
                     "geometry": {
                         "type": "Point",
-                        coordinates: [0.0, 0.0]
+                        "coordinates": [0.0, 0.0]
                     }
                 }
             }
@@ -107,6 +107,58 @@ export function createMap() {
 
     layer.add(boundary, map, true, init);
     info.setUp(map);
+
+    // The 'building' layer in the streets vector source contains building-height
+    // data from OpenStreetMap.
+    map.on('load', () => {
+        // Insert the layer beneath any symbol layer.
+        const layers = map.getStyle().layers;
+
+        let labelLayerId;
+        for (let i = 0; i < layers.length; i++) {
+            if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+                labelLayerId = layers[i].id;
+                break;
+            }
+        }
+
+        map.addSource('openfreemap', {
+            url: `https://tiles.openfreemap.org/planet`,
+            type: 'vector',
+        });
+
+        map.addLayer(
+            {
+                'id': '3d-buildings',
+                'source': 'openfreemap',
+                'source-layer': 'building',
+                'type': 'fill-extrusion',
+                'minzoom': 15,
+                'filter': ['!=', ['get', 'hide_3d'], true],
+                'paint': {
+                    'fill-extrusion-color': [
+                        'interpolate',
+                        ['linear'],
+                        ['get', 'render_height'], 0, 'lightgray', 200, 'royalblue', 400, 'lightblue'
+                    ],
+                    'fill-extrusion-height': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        15,
+                        0,
+                        16,
+                        ['get', 'render_height']
+                    ],
+                    'fill-extrusion-base': ['case',
+                        ['>=', ['get', 'zoom'], 16],
+                        ['get', 'render_min_height'], 0
+                    ]
+                }
+            },
+            labelLayerId
+        );
+    });
 
     return map;
 }
